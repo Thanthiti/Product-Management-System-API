bddf13e63c78817b9a245557eea5065e90ad53f9
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-10-05 19:53:24 +0700

    initial project

---
+++ src/main/java/com/thanthiti/product_system/ProductSystemApplication.java
@@ -0,0 +1,14 @@
+package com.thanthiti.product_system;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSystemApplication.class, args);
+	}
+}
+
+
